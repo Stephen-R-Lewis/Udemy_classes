02a10dae65da9e4d3362a073b3b234c1f80c7fa0
Stephen Lewis <st.rayis1085.yahoo.com>
2024-10-20 15:10:31 -0400

    finally took me all weekend

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -43,26 +43,61 @@ This is the Server
 //knowing what runs once and what runs multiple times is key to knowingwhen to 
 //split things and use async and sync properly. Hence this dataObj is only read
 // once so it gets its nifty little varible
+
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%CHANGETHIS%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8")
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8")
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8")
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
+    //morgan
     console.log(req.url)
-
     const pathName = req.url
 
     switch (pathName) {
+        //this is home page
         case "/":
-        case "/overView":
-            res.end("This is the OVERVIEW")
+        case "/overview":
+            res.writeHead(200, { "Content-type": "text/html" })
+
+            const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+            const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+            res.end(output);
             break;
+
+        //product page
         case "/product":
             res.end("This is the PRODUCT")
             break;
+
+        //API
         case "/api":
-                res.writeHead(200, { "Content-type": "application/json" })
+            res.writeHead(200, { "Content-type": "application/json" })
             res.end(data)
             break;
+
+        //ERR page
         default:
             res.writeHead(404, {
                 "Content-type": "text/html",
@@ -71,7 +106,14 @@ const server = http.createServer((req, res) => {
             res.end(`<h1>"This page could not be found?"</h1>`)
     }//end of switch
 })
-    
+
+//these are actual default values
+server.listen(8001, "127.0.0.1", () => {
+    console.log("listening at port 8001")
+})
+
+
+
 //     if (pathName === "/" || pathName === "/overView") {
 //         res.end("This is the OVERVIEW")
 //     } else if (pathName === "/product") {
@@ -85,8 +127,3 @@ const server = http.createServer((req, res) => {
 //         res.end(`<h1>"This page could not be found?"</h1>`)
 //     }
 // });
-
-//these are actual default values
-server.listen(8000, "127.0.0.1", () => {
-    console.log("listening at port 8000")
-})
